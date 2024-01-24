714b551d84443144de83d6ca4749d6fc4c29a7c7
lemi1 <lemi1@192.168.0.13>
2024-01-24 11:28:04 +0900

    Ch01_helloWorld 프로젝트 생성

---
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -0,0 +1,9 @@
+package ch01.hello;
+
+public class Hello {
+	// 프로그램에서 프로그램 시작 부분을 main으로 정해놓음
+	// ctrl + f11 하면 실행이 됨
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
