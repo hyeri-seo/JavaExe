fd9c0963016fc4e6e4cf2d470172c43c49097f27
서혜리 <hye_dam_2@naver.com>
2024-01-25 16:11:48 +0900

    WhileEx.java While 조건문 예제

---
+++ Ch03_Control/src/ch03/while08/WhileEx.java
@@ -0,0 +1,13 @@
+package ch03.while08;
+
+public class WhileEx {
+
+	public static void main(String[] args) {
+		int i = 0;							//초기식
+		while(i<10) {						//조건(종료:언제까지)
+			i++;							//증감식(변화:얼마만큼)
+			System.out.print(i + ", ");	//실행문
+		}
+	}
+
+}
