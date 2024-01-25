aa264943e926b698adfc17709d300560b1f488de
서혜리 <hye_dam_2@naver.com>
2024-01-25 17:14:28 +0900

    ForEx.java for문 예제(while문과 비교)

---
+++ Ch03_Control/src/ch03/for10/ForEx.java
@@ -0,0 +1,13 @@
+package ch03.for10;
+
+public class ForEx {
+
+	public static void main(String[] args) {
+		//1~10 출력
+		int i = 1;					//초기
+		for(;i<=10;) {				//조건
+			System.out.println(i);	//실행
+			i++;					//증가
+		}
+	}
+}
---
+++ Ch03_Control/src/ch03/for10/ForEx1.java
@@ -0,0 +1,18 @@
+package ch03.for10;
+
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		//1~10 출력
+//		int i = 1;					//초기
+//		for(;i<=10;) {				//조건
+//			System.out.println(i);	//실행
+//			i++;					//증가
+//		}
+		
+		//한 번만 실행;조건(계속 비교);실행이 끝난 후 값 변화
+		for(int i=1;i<=10;i++) {	//시작, 끝, 변화량
+			System.out.println(i);	//실행
+		}
+	}
+}
