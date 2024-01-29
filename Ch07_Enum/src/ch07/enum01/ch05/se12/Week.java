15749c57da25bcaab02689fda6e14ca55ed43883
서혜리 <hye_dam_2@naver.com>
2024-01-29 17:32:37 +0900

    enum 예제

---
+++ Ch07_Enum/src/ch07/enum01/ch05/se12/Week.java
@@ -0,0 +1,13 @@
+package ch07.enum01.ch05.se12;
+
+public enum Week {
+	//enum은 프로그래밍의 가독성을 높여주는 문법
+	//내부적으로 차례대로 0부터 1씩 증가하는 값을 갖고 있음
+	MONDAY,			//1
+	TUESDAY,		//2
+	WEDNESDAY,		//3
+	THURSDAY,		//4
+	FRIDAY,			//5
+	SATURDAY,		//6
+	SUNDAY			//7
+}
