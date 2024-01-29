d9e97f5711985a5be0f36a1432c675c8b29656cb
서혜리 <hye_dam_2@naver.com>
2024-01-29 09:38:56 +0900

    Resolve2 수정

--- Ch05_Array/src/ch05/homework/Resolve2.java
+++ Ch05_Array/src/ch05/homework/Resolve2.java
@@ -4,16 +4,14 @@ public class Resolve2 {
 //1. 정수 배열 5개를 할당하고 Arrays.sort를 활용하지 말고 최소값과 최대값을 얻으세요
 	public static void main(String[] args) {
 		int[] arr = {50, 60, 10, 70, 100};
-		int temp;
-		for(int i=0; i<arr.length-1; i++) {
-			if(arr[i] > arr[i+1]) {
-				temp = arr[i+1];
-				arr[i+1] = arr[i];
-				arr[i] = temp;
-			}
-		}
 		int min = arr[0];
-		int max = arr[arr.length-1];
+		int max = arr[arr.length -1];
+		for(int i=0; i<arr.length-1; i++) {
+			if(arr[i] < min) min = arr[i];
+		}
+		for(int i=0; i<arr.length-1; i++) {
+			if(arr[i] > max) max = arr[i];
+		}
 		System.out.println("최소값: " + min);
 		System.out.println("최대값: " + max);
 	}
