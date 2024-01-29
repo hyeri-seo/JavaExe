aaa8056343a5ef45cd5c8f7f5649dd6ace77b955
서혜리 <hye_dam_2@naver.com>
2024-01-29 16:16:29 +0900

    메소드명 수정

--- Ch06_Method/src/ch06/method01/Resolve2.java
+++ Ch06_Method/src/ch06/method01/Resolve2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 //사용해보세요. 메서드 호출 시 전달되는 값의 순서와 상관없이
 //절대값이 계산되어서 출력되어야 합니다
 public class Resolve2 {
-	public static int minus(int x, int y) {
+	public static int absoluteValue(int x, int y) {
 		if(x >= y) return x - y;
 		else return y - x;
 	}
@@ -17,7 +17,7 @@ public class Resolve2 {
 		int x = sc.nextInt();
 		System.out.print("2번째 수를 입력해주세요. >> ");
 		int y = sc.nextInt();
-		int result = minus(x, y);
+		int result = absoluteValue(x, y);
 		System.out.println("두 수의 차의 절대값: " + result);
 		sc.close();
 	}
