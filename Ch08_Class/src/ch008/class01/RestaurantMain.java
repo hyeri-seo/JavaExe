35ae21021589acba9a987a4de1dba0db02745d76
서혜리 <hye_dam_2@naver.com>
2024-01-30 14:33:55 +0900

    설명 추가

--- Ch08_Class/src/ch008/class01/RestaurantMain.java
+++ Ch08_Class/src/ch008/class01/RestaurantMain.java
@@ -2,6 +2,11 @@ package ch008.class01;
 
 public class RestaurantMain {
 	public static void main(String[] args) {
+		/*
+		 * 클래스도 자료형
+		 * 우리가 직접 만든 사용자 정의 자료형
+		 */
+		//클래스 객체(클래스의 변수) = new(힙에 할당한다) 클래스의 생성자 함수 호출;
 		Restaurant rest = new Restaurant();
 		rest.viewMenu();
 		rest.selectFood();
