36a6430d19bf415558b950d2399536085a5cd17a
서혜리 <hye_dam_2@naver.com>
2024-01-30 16:43:19 +0900

    Dao -> Dto 수정

--- Ch08_Class/src/ch008/class01/Restaurant.java
+++ Ch08_Class/src/ch008/class01/Restaurant.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  * [클래스의 대상]
  * 큰 개념의 명사(일반적인 클래스)
  * 동사들의 집합(Controller, Business layer)
- * 필드들의 집합(Vo, Dao)
+ * 필드들의 집합(Vo, Dto)
  */
 public class Restaurant {
 	// 필드
