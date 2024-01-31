08b79e81670a1ee302ae7840e7f0de684f60674d
서혜리 <hye_dam_2@naver.com>
2024-01-31 16:13:11 +0900

    상속을 했을 때 부모 생성자 호출

---
+++ Ch09_Extends/src/ch09/extends03/ExtendsMain.java
@@ -0,0 +1,19 @@
+package ch09.extends03;
+
+public class ExtendsMain {
+	public static void main(String[] args) {
+		Child child = new Child();
+		
+		//메소드의 상속
+		child.play();
+		child.wealth();
+		child.handsome();
+		
+		System.out.println();
+		
+		//필드의 상속
+		System.out.println("잘생김 점수: " + child.handsomeScore);
+		System.out.println("돈: " + child.money);
+		System.out.println("즐거운 날: " + child.day);
+	}
+}
