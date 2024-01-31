1415ccd74548468e888b19825b727502c74200d2
서혜리 <hye_dam_2@naver.com>
2024-01-31 18:01:27 +0900

    1번 문제 풀이

---
+++ Ch09_Extends/src/ch09/Resolve07/Resolve1/Add.java
@@ -0,0 +1,16 @@
+package ch09.Resolve07.Resolve1;
+
+public class Add{
+	
+	public int a, b;
+
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public int calculate() {
+		return a + b;
+	}
+
+}
---
+++ Ch09_Extends/src/ch09/Resolve07/Resolve1/Mul.java
@@ -0,0 +1,15 @@
+package ch09.Resolve07.Resolve1;
+
+public class Mul{
+
+	public int a, b;
+
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public int calculate() {
+		return a * b;
+	}
+}
