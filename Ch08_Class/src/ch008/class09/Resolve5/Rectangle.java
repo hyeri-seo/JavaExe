26f147da9570a2519e491f2f738b17fb70b3ed29
서혜리 <hye_dam_2@naver.com>
2024-02-01 12:36:16 +0900

    강사님 풀이 추가

--- Ch08_Class/src/ch008/class09/Resolve5/Rectangle.java
+++ Ch08_Class/src/ch008/class09/Resolve5/Rectangle.java
@@ -13,6 +13,11 @@ public class Rectangle {
 	}
 	
 	public int square(Rectangle r) {
+//		int width = Math.abs(x1-x2);
+//		int height = Math.abs(y1-y2);
+//		
+//		int area = width*height;
+		
 		int area = ((r.x1 - r.x2)*(r.x1 - r.x2) + (r.y1 - r.y2)*(r.y1 - r.y2))/2;
 		return area;
 	}
@@ -22,11 +27,29 @@ public class Rectangle {
 		System.out.printf("직삭각형을 이루고 있는 점2의 좌표:(%d, %d)\n", x2, y2);
 		System.out.println("사각형의 넓이 = " 
 		+ ((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2))/2);
+		
+//		int leftX = x1<x2 ? x1 : x2;
+//		int topY = y1<y2 ? y1 : y2;
+//		int rightX = x1>x2 ? x1 : x2;
+//		int bottomY = y1<y2 ? y1 : y2;
+//		
+//		System.out.println("좌상단 x1 = " +leftX);
+//		System.out.println("좌상단 y1 = " +topY);
+//		System.out.println("우하단 x2 = " +rightX);
+//		System.out.println("우하단 y2 = " +topY);
 	}
 	
 	public boolean equals(Rectangle r) {
 		if(r.x1 == x1 && r.y1 == y1 &&
 				r.x2 == x2 && r.y2 == y2) return true;
 		else return false;
+		
+//		if(this.x1 == r.x1 &&
+//			this.y1 == r.y1 &&
+//			this.x2 == r.x2 &&
+//			this.y2 == r.y2) {
+//			return true;
+//		}
+//		return false;
 	}
 }
