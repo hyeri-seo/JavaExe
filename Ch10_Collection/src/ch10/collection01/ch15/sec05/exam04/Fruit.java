defe23239c3616f269e6b736b9ce148eaeeccb53
서혜리 <hye_dam_2@naver.com>
2024-02-06 10:10:28 +0900

    Collection Framework

---
+++ Ch10_Collection/src/ch10/collection01/ch15/sec05/exam04/Fruit.java
@@ -0,0 +1,13 @@
+package ch10.collection01.ch15.sec05.exam04;
+
+public class Fruit {
+	public String name;
+	public int price;
+	
+	public Fruit(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+}
