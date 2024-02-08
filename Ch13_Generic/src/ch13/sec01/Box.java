40e813c33cbb7b2a2a71cb8d40f6ff795f3f130e
서혜리 <hye_dam_2@naver.com>
2024-02-08 11:17:33 +0900

    Generic 설명 추가

--- Ch13_Generic/src/ch13/sec01/Box.java
+++ Ch13_Generic/src/ch13/sec01/Box.java
@@ -1,5 +1,11 @@
 package ch13.sec01;
 
+/*
+ * T는 대명사
+ * E, A, MY, HELLO, ... 모두 가능함
+ * T는 자료형을 프로그래머가 직접 선택하라는 의미임
+ * 다만 T는 (Type)을 의미함
+ */
 public class Box<T> {
 	public T content;
 }
--- Ch13_Generic/src/ch13/sec01/GenericExample.java
+++ Ch13_Generic/src/ch13/sec01/GenericExample.java
@@ -1,5 +1,18 @@
 package ch13.sec01;
 
+/*
+ * Box<String> box1 = new Box<>();
+ * Box<Integer> box2 = new Box<>();
+ * 
+ * 위처럼 코드를 자것앟면 아래처럼 컴파일러는 자동으로 클래스 2개를 정의하게 됨
+ * class Box_String {
+ * 	public String content;
+ * }
+ * class Integer {
+ * 	public Integer content;
+ * }
+ */
+
 public class GenericExample {
 
 	public static void main(String[] args) {
@@ -11,8 +24,8 @@ public class GenericExample {
 
 		// Box<Integer> box2 = new Box<Integer>();
 		Box<Integer> box2 = new Box<>();
-		box2.content = 100;
-		int value = box2.content;
+		box2.content = 100;			//Boxing: Heap -> Stack
+		int value = box2.content;	//UnBoxing: Stack -> Heap
 		System.out.println(value);
 	}
 
