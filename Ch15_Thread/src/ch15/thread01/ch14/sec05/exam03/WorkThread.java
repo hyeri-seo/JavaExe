60ef834690fe743144ce1f7b811e481c07475034
서혜리 <hye_dam_2@naver.com>
2024-02-13 14:39:07 +0900

    멀티스레드

--- Ch15_Thread/src/ch15/thread01/ch14/sec05/exam03/WorkThread.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec05/exam03/WorkThread.java
@@ -9,6 +9,17 @@ public class WorkThread extends Thread {
 		setName(name);
 	}
 	
+	/*
+	 * 스레드의 제어 권한을 다른 스레드에 넘기는 방법
+	 * 1) sleep(10);
+	 * 	  10ms 동안 현재 스레드는 스케줄링에서 제외하겠다
+	 * 	  즉, 다른 스레드에 실행을 넘기겠다.
+	 * 	  난 멈추겠다.
+	 * 2) Thread.yield();
+	 * 	  의미없는 일을 하느니 그냥 넘기겠다.
+	 * 	  즉, 다른 스레드에 실행을 넘기겠다.
+	 */
+	
 	//메소드
 	@Override
 	public void run() {
